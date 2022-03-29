f6146a842fcf4bd8893165193bdee120336099a2
Emma Janson <emmajanson@hotmail.se>
2022-03-29 11:38:15 +0200

    smartasses is here

--- RecipeApp-main/src/pages/Searched.jsx
+++ RecipeApp-main/src/pages/Searched.jsx
@@ -3,9 +3,11 @@ import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {Link} from "react-router-dom";
 import './Searched.css';
+import Loader from '../components/Loader';
 
 function Searched() {
     const [searchedRecipes, setSearchedRecipes] = useState([]);
+    const [searchIsLoading, setSearchIsLoading] = useState(true);
     let params = useParams();
 
     const getSearched = async (name) => {
@@ -14,28 +16,31 @@ function Searched() {
         );
         const recipes = await data.json();
         setSearchedRecipes(recipes.results);
+        setSearchIsLoading(false);
     };
 
     useEffect(() => {
         getSearched(params.search);
     }, [params.search]);
 
-  return (
+  return searchIsLoading ? (
+    <Loader />
+  ) : (
     <div className='searchedWrapper'>
-      <div className='searchedGrid'>
-        {searchedRecipes.map((item) => {
-            return (
-                <div className='searchedCard' key={item.id}>
-                  <Link className='searchedLink' to={'/recipe/' + item.id}>
-                    <img className='searchedImage' src={item.image} alt="" />
-                    <h4 className='searchedItemTitle'>{item.title}</h4>
-                  </Link>
-                </div>
-            );
-        })}
-      </div>
+    <div className='searchedGrid'>
+      {searchedRecipes.map((item) => {
+          return (
+              <div className='searchedCard' key={item.id}>
+                <Link className='searchedLink' to={'/recipe/' + item.id}>
+                  <img className='searchedImage' src={item.image} alt="" />
+                  <h4 className='searchedItemTitle'>{item.title}</h4>
+                </Link>
+              </div>
+          );
+      })}
     </div>
-  );
+  </div>
+  )
 }
 
 export default Searched;
