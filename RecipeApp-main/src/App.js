147c5d5ebfed7e730ce4484e5677669a02f29b1e
Lukas-i-bastun <lukash@live.se>
2022-03-29 13:01:54 +0200

    app.js update

--- RecipeApp-main/src/App.js
+++ RecipeApp-main/src/App.js
@@ -1,20 +1,50 @@
 import Pages from "./pages/Pages";
+import react, { useState, useEffect } from "react";
 import Category from "./components/Category";
 import Search from "./components/Search";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
+
+// MOBILE MODE START
+  const [mobileMode, setMobileMode] = useState(false)
+  const [windowLoad, setWindowLoad] = useState(false)
+
+  useEffect(() => {
+    if (window.innerWidth < 500) {
+      setWindowLoad(true)
+    } else if (window.innerWidth > 500) {
+      setWindowLoad(false)
+    }
+  }, [])
+
+  window.addEventListener('resize', () => {
+
+    if (window.innerWidth < 500) {
+      setMobileMode(true)
+    } else if (window.innerWidth > 500) {
+      setMobileMode(false)
+    }
+   })
+
+   useEffect(() => {
+  console.log(mobileMode);
+   }, [mobileMode])
+
+// MOBILE MODE END
+ 
+
+
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Category />
         <Search />
-        <Pages />
+        <Pages mobileMode={mobileMode} windowLoad={windowLoad}  />
       </BrowserRouter>
     </div>
-    // Hej hej har skriver jag 
   );
 }
 
