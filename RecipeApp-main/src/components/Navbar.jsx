0bed2ad76d1ae7124dd5cc373b0d8bcfbf88d87e
Emma Janson <emmajanson@hotmail.se>
2022-03-30 19:54:33 +0200

    update

--- RecipeApp-main/src/components/Navbar.jsx
+++ RecipeApp-main/src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { GiKnifeFork } from "react-icons/gi";
 import { Link } from "react-router-dom";
-import '../style/Header.css';
+import '../style/Navbar.css';
 
-function Header() {
+function Navbar() {
   return (
     <div className='navbar'>
       <div className='logo'>
         <Link className="icon" to={"/"}><GiKnifeFork /> Yummyyy </Link>
       </div>
       <div className='navbar_links'>
-        <Link className='Home' to={"/"}> Home </Link>
+        <Link className='home' to={"/"}> Home </Link>
       </div>
     </div>
   )
 }
 
-export default Header;
+export default Navbar;
